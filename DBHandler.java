e98f27422e7c8fe77442986c4950402af5d1a877
opi <j070604@gmail.com>
2015-06-06 22:15:47 +0900

    DB에 access 하여 관련 기능들을 수행하는 DBHandler 클래스를 추가하고 기존 ExtractHandler에 존재하는 DB 관련 기능들을 분리시킴.

---
+++ DBHandler.java
@@ -0,0 +1,236 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+
+public class DBHandler {
+	
+	Connection con = null;
+	
+	private String url = "jdbc:mysql://dblp-db.cbrenledlob9.ap-northeast-1.rds.amazonaws.com";;
+	private String user = "KW";
+	private String pass = "dblp2015";
+	
+	private static final int KEY = 1; // 
+	private static final int TYPE = 2;
+	private static final int TITLE = 3;
+	private static final int BOOKTITLE = 4;
+	private static final int YEAR = 5;
+	private static final int JOURNAL = 6;
+	private static final int VOLUME = 7;
+	private static final int MONTH = 8;
+	private static final int NOTE = 9;
+	private static final int SERIES = 10;
+	private static final int URL = 11;
+	private static final int EE = 12;
+	private static final int COAUTHOR = 13;
+	
+	/*
+	 * GetServerAccountInto()
+	 * 아마존 서버뿐만이 아니라 다른 서버를 사용하고 싶을 때
+	 * 입력을 통해 새로운 서버에 접속할 수 있도록 해주는 input 함수
+	 * 단 다른 서버의 db, table의 이름도 아마존꺼랑 같아야함
+	 */
+	private void GetServerAccountInto()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("아마존 계정 사용 -> 아무거나 입력 \n개인 계정 사용 ->q 입력");
+		String accountSelection = sc.nextLine();
+		
+		if("q".compareTo(accountSelection) == 0 || "Q".compareTo(accountSelection) == 0)
+		{
+			//사설계정 이용하신 답니다.
+			System.out.print("input your url : ");
+			url = "jdbc:mysql://" + sc.nextLine();
+			System.out.print("input your user ID : ");
+			user = sc.nextLine();
+			System.out.print("input your user PASSWORD : ");
+			pass = sc.nextLine();
+		}
+		
+		sc.close();
+	}
+	
+	/*
+	 * ConnectDB()
+	 * DB에 접속하기만 할 뿐인 함수
+	 */
+	public void ConnectDB()
+	{
+		GetServerAccountInto();
+		
+		try
+		{
+			Class.forName("org.gjt.mm.mysql.Driver");
+			System.out.println("드라이버 검색 성공!!");	
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("드라이버 검색 실패!");
+			return;
+		}
+		
+		try
+		{
+			con = DriverManager.getConnection(url,user,pass);
+			System.out.println("My-SQL 접속 성공!!");
+		}
+		catch(SQLException e)
+		{
+			System.out.println("My-SQL 접속 실패");
+		}
+	}
+
+	/*
+	 * InsertETC(ArrayList<XmlElements> xeList)
+	 * 다량의 XmlElements 들을 DB insert시
+	 * InsertETC 자체를 여러번 호출하는게 아니라 
+	 * XmlElements를 ArrayList의 형태로 전달하여
+	 * insert를 반복해주도록 오버로딩된 함수
+	 */
+	public void InsertETC(ArrayList<XmlElements> xeList)
+	{
+		for(int i = 0; i < xeList.size(); ++i)
+		{
+			InsertETC(xeList.get(i));
+		}
+	}
+	
+	/*
+	 * InsertETC(XmlElements tag)
+	 * XmlElements 하나를 인자로 받아 DB ETC table에
+	 * insert 해주는 함수
+	 */
+	public void InsertETC(XmlElements tag)
+	{
+		int n = 0;
+		
+		//etc 테이블부터 채워보자
+		PreparedStatement ps = null;
+		String sql = "insert into dblp.etc values(?,?,?,?,?,?,?,?,?,?,?,?,?)"; //쿼리문
+		//key, type, title, booktitle, year, journal, volume, month, note, series,q url, ee, coauthor
+		
+		try
+		{
+			ps = con.prepareStatement(sql);
+			ps.setString(KEY, tag.key); //키값
+			ps.setString(TYPE, tag.type); //type
+			ps.setString(TITLE, tag.title); //title
+			ps.setString(BOOKTITLE, tag.booktitle); //booktitle
+			ps.setInt(YEAR, tag.year); //year
+			ps.setString(JOURNAL, tag.journal); //journal
+			ps.setInt(VOLUME, tag.volume); //volume
+			ps.setString(MONTH, tag.month); //month
+			ps.setString(NOTE, tag.note); //note
+			ps.setString(SERIES, tag.series); //시리즈
+			ps.setString(URL, tag.url); //url
+			ps.setString(EE, tag.ee); //ee
+			ps.setString(COAUTHOR, tag.coauthor); //공동저자
+			
+			
+			//퀴리문과 현재 tag의 값 출력
+			/*
+			System.out.println(ps);
+			System.out.println("1." + tag.key + " 2." + tag.type + " 3." + tag.title + " 4." + tag.booktitle + " 5." + tag.year
+					 + " 6." + tag.journal + " 7." + tag.volume + " 8." + tag.month + " 9." + tag.note + " 10." + tag.series
+					 + " 11." + tag.url + " 12." + tag.ee + " 13." + tag.coauthor);
+			
+			System.out.println();
+			*/
+			
+			n = ps.executeUpdate(); //데이터 삽입
+			
+			if(n<=0)
+			{
+				System.out.println("etc table 데이터추가 실패");
+				return;
+			}
+		}
+		/* 키값 중복 예외등이 발생시 하는 예외*/
+		catch(MySQLIntegrityConstraintViolationException e) 
+		{
+			e.printStackTrace();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		//author table 데이터 추가
+		/*
+		int i;
+		ps = null; //초기화
+		sql = "insert into DBLP_DB.author values(?,?)"; //쿼리문
+		
+		try{
+			ps = con.prepareStatement(sql);
+			ps.setString(1,tag.key); //키값
+			
+			for(i=0;i<tag.authors.size(); i++)
+			{//리스트에 있는만큼 저자들을 추가
+				ps.setString(2,tag.authors.get(i)); //type
+
+				n = ps.executeUpdate(); //데이터 삽입
+				if(n<=0){
+					System.out.println("author table 데이터추가 실패");
+					return;
+				}
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		
+		//cite 테이블
+		ps = null; //초기화
+		sql = "insert into DBLP_DB.cite values(?,?)"; //쿼리문
+		
+		try{
+			ps = con.prepareStatement(sql);
+			ps.setString(1,tag.key); //키값
+			
+			for(i=0;i<tag.cites.size(); i++)
+			{//리스트에 있는만큼 인용정보를 추가
+				ps.setString(2,tag.cites.get(i)); //type
+
+				n = ps.executeUpdate(); //데이터 삽입
+				if(n<=0){
+					System.out.println("cite table 데이터추가 실패");
+					return;
+				}
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		
+		//editor 테이블
+		ps = null; //초기화
+		sql = "insert into DBLP_DB.editor values(?,?)"; //쿼리문
+		
+		try{
+			ps = con.prepareStatement(sql);
+			ps.setString(1,tag.key); //키값
+			
+			for(i=0;i<tag.editors.size(); i++)
+			{//리스트에 있는만큼 인용정보를 추가
+				ps.setString(2,tag.editors.get(i)); //type
+
+				n = ps.executeUpdate(); //데이터 삽입
+				if(n<=0){
+					System.out.println("editor table 데이터추가 실패");
+					return;
+				}
+			}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}*/
+
+	}
+	
+	
+}
--- ExtractHandler.java
+++ ExtractHandler.java
@@ -1,9 +1,6 @@
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.sql.*;
-
-//import jdk.internal.org.xml.sax.SAXException;
 
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.Attributes;
@@ -15,9 +12,9 @@ public class ExtractHandler extends DefaultHandler{
 	Map<String, PutCharToXmlElements> map;
 	String data;
 	
-	Connection con = null;
+	DBHandler dbh;
 	
-	public ExtractHandler()
+	private void InitHS()
 	{
 		hs = new HashSet<>(); //init HashSet - has dblp element
 		hs.add("article");
@@ -28,7 +25,10 @@ public class ExtractHandler extends DefaultHandler{
 		hs.add("phdthesis");
 		hs.add("mastersthesis");
 		hs.add("www");
-		
+	}
+	
+	private void InitHM()
+	{
 		map = new HashMap<String, PutCharToXmlElements>();
 		
 		map.put("author", new PutCharToXmlElements() {
@@ -122,36 +122,32 @@ public class ExtractHandler extends DefaultHandler{
 		});
 	}
 	
+	public ExtractHandler()
+	{
+		InitHS();
+		InitHM();
+		
+		dbh = new DBHandler();
+	}
+	
 	
 	public boolean isElement(String localName)
 	{
 		return hs.contains(localName);
 	}
 	
-	public void startDocument() {
-		
+	public void startDocument() 
+	{	
 		System.out.println("XML 문서 시작.");
 		System.out.println("Database 접속 시작.");
 		
-		try{
-			Class.forName("org.gjt.mm.mysql.Driver");
-			System.out.println("드라이버 검색 성공!!");	
-		}catch(ClassNotFoundException e){
-			System.out.println("드라이버 검색 실패!");
-			return;
-		}
-		
-		String url = "jdbc:mysql://dblp-db.cbrenledlob9.ap-northeast-1.rds.amazonaws.com";
-		String user = "KW";
-		String pass = "dblp2015";
-		try{
-			con = DriverManager.getConnection(url,user,pass);
-			System.out.println("My-SQL 접속 성공!!");
-		}catch(SQLException e){
-			System.out.println("My-SQL 접속 실패");
-		}
+		dbh.ConnectDB();
 	}
 	
+	/*
+	 * endDocument : 문서가 끝이 날 때 호출되는 이벤트 함수
+	 * 하는일 : 아직 음슴. 메모리 정리정도로 사용 가능할 듯
+	 * */
 	public void endDocument() {}
 	
 	public void startElement(String uri, String localName, String qname, Attributes attr)
@@ -171,129 +167,7 @@ public class ExtractHandler extends DefaultHandler{
 		{
 			//end of get one element's info so call the DB insert Funtion
 			// 여기서 tag 변수는 xml정보를 모두 담고 있으므로 가져다가 insert 하면 됨
-			
-			
-			int n;
-			
-			//etc 테이블부터 채워보자
-			PreparedStatement ps = null;
-			String sql = "insert into DBLP_DB.etc values(?,?,?,?,?,?,?,?,?,?,?,?,?)"; //쿼리문
-			//key, type, title, booktitle, year, journal, volume, month, note, series, url, ee, coauthor
-			
-			try{
-				ps = con.prepareStatement(sql);
-				ps.setString(1,tag.key); //키값
-				ps.setString(2,tag.type); //type
-				ps.setString(3,tag.title); //title
-				ps.setString(4,tag.booktitle); //booktitle
-				ps.setInt(5,tag.year); //year
-				ps.setString(6,tag.journal); //journal
-				ps.setInt(7,tag.volume); //volume
-				ps.setString(8,tag.month); //month
-				ps.setString(9,tag.note); //note
-				ps.setString(10,tag.series); //시리즈
-				ps.setString(11,tag.url); //url
-				ps.setString(12,tag.ee); //ee
-				ps.setString(13,tag.coauthor); //공동저자
-				
-				
-				//퀴리문과 현재 tag의 값 출력
-				System.out.println(ps);
-				System.out.println("1." + tag.key + " 2." + tag.type + " 3." + tag.title + " 4." + tag.booktitle + " 5." + tag.year
-						 + " 6." + tag.journal + " 7." + tag.volume + " 8." + tag.month + " 9." + tag.note + " 10." + tag.series
-						 + " 11." + tag.url + " 12." + tag.ee + " 13." + tag.coauthor);
-				
-				System.out.println();
-				
-				/*
-				n = ps.executeUpdate(); //데이터 삽입
-				if(n<=0){
-					System.out.println("etc table 데이터추가 실패");
-					return;
-				}*/
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-			}
-			
-			//author table 데이터 추가
-			/*
-			int i;
-			ps = null; //초기화
-			sql = "insert into DBLP_DB.author values(?,?)"; //쿼리문
-			
-			try{
-				ps = con.prepareStatement(sql);
-				ps.setString(1,tag.key); //키값
-				
-				for(i=0;i<tag.authors.size(); i++)
-				{//리스트에 있는만큼 저자들을 추가
-					ps.setString(2,tag.authors.get(i)); //type
-
-					n = ps.executeUpdate(); //데이터 삽입
-					if(n<=0){
-						System.out.println("author table 데이터추가 실패");
-						return;
-					}
-				}
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-			}
-			
-			
-			//cite 테이블
-			ps = null; //초기화
-			sql = "insert into DBLP_DB.cite values(?,?)"; //쿼리문
-			
-			try{
-				ps = con.prepareStatement(sql);
-				ps.setString(1,tag.key); //키값
-				
-				for(i=0;i<tag.cites.size(); i++)
-				{//리스트에 있는만큼 인용정보를 추가
-					ps.setString(2,tag.cites.get(i)); //type
-
-					n = ps.executeUpdate(); //데이터 삽입
-					if(n<=0){
-						System.out.println("cite table 데이터추가 실패");
-						return;
-					}
-				}
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-			}
-			
-			
-			//editor 테이블
-			ps = null; //초기화
-			sql = "insert into DBLP_DB.editor values(?,?)"; //쿼리문
-			
-			try{
-				ps = con.prepareStatement(sql);
-				ps.setString(1,tag.key); //키값
-				
-				for(i=0;i<tag.editors.size(); i++)
-				{//리스트에 있는만큼 인용정보를 추가
-					ps.setString(2,tag.editors.get(i)); //type
-
-					n = ps.executeUpdate(); //데이터 삽입
-					if(n<=0){
-						System.out.println("editor table 데이터추가 실패");
-						return;
-					}
-				}
-			}
-			catch(SQLException e){
-				e.printStackTrace();
-			}*/
-			
-			System.out.println("1." + tag.key + " 2." + tag.type + " 3." + tag.title + " 4." + tag.booktitle + " 5." + tag.year
-					 + " 6." + tag.journal + " 7." + tag.volume + " 8." + tag.month + " 9." + tag.note + " 10." + tag.series
-					 + " 11." + tag.url + " 12." + tag.ee + " 13." + tag.coauthor);
-			
-			
+			dbh.InsertETC(tag);
 			return;
 		}
 		try
